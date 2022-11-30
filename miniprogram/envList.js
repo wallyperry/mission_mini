95e1341489f8b37b2fd20e959352afdfc6e96aac
Perry <pl.w@outlook.com>
2022-11-30 14:33:11 +0800

    修复时间显示问题

---
+++ miniprogram/envList.js
@@ -0,0 +1,4 @@
+const envList = [{"envId":"tasks-8grawjva83cd0c12","alias":"cloud"}]
+module.exports = {
+    envList,
+}
---
+++ miniprogram/utils.js
@@ -0,0 +1,24 @@
+let Util = {
+    /**
+     * 格式化日期时间
+     * @param {date} date 日期时间
+     * @param {string} fmt 格式,如：'yyyy-MM-dd HH:mm:ss'
+     */
+    formatDate (date, fmt) {
+      if (!date) return '-'
+      date = new Date(date)
+      const o = {
+        'M+': date.getMonth() + 1, // 月份
+        'd+': date.getDate(), // 日
+        'H+': date.getHours(), // 小时
+        'm+': date.getMinutes(), // 分
+        's+': date.getSeconds(), // 秒
+        'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+        S: date.getMilliseconds() // 毫秒
+      }
+      if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+      for (const k in o) { if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length))) }
+      return fmt
+    }
+  }
+  export default Util
